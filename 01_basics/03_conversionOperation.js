dc43b139d462b36e86785471dcbdf36102b09c9e
Mohd Sadiq Ameen <mohdsadiqameen989@gmail.com>
2024-01-26 16:36:03 +0000

    operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,28 +1,43 @@
-let score = false;
-console.log("Old value = " + score);
-console.log("typeof Old = " + typeof(score));
-// Conversion to Number
-// when string => number , it value = NaN , eg : "33abc" = NaN , " sadiq" = NaN
-// null => number , conversion when happen , null => 0
-// undefined => number , value = NaN  
-// true => 1 , false => 0 , convert to 
-// Conversion to boolean
-// 0 = false , 1 & any number = true
-// " " => true , "string" = true
-let neWw = Boolean(score);
-console.log("New value = " + neWw);
-console.log("typeof New = " + typeof neWw);
-// Converion to string
-// 32 => 32 , type ofcourse string
-// boolean => string , true => true , false => false
-let neW = String(score);
-console.log("New value = " + neW);
-console.log("typeof New = " + typeof neW);
-// *********************** Operations **********************************************
+// let score = false;
+
+// console.log("Old value = " + score);
+// console.log("typeof Old = " + typeof(score));
+
+// // Conversion to Number
+// // when string => number , it value = NaN , eg : "33abc" = NaN , " sadiq" = NaN
+// // null => number , conversion when happen , null => 0
+// // undefined => number , value = NaN  
+// // true => 1 , false => 0 , convert to 
+
+
+// // Conversion to boolean
+
+// // 0 = false , 1 & any number = true
+// // " " => true , "string" = true
+
+
+// let neWw = Boolean(score);
+
+// console.log("New value = " + neWw);
+// console.log("typeof New = " + typeof neWw);
+
+
+// // Converion to string
+// // 32 => 32 , type ofcourse string
+// // boolean => string , true => true , false => false
+
+
+// let neW = String(score);
+
+// console.log("New value = " + neW);
+// console.log("typeof New = " + typeof neW);
+
+
+// *********************** Operations ************************************************
 
 console.log("2" + 1 + 1);      // output = 211
 console.log( 1 + 1 + "2" );    // output = 22
 
 let i = 0;
 console.log(i++);    // print = 0 , but value = 1
-console.log(++i);    // print = 2 , because value bec 1 in previous
+console.log(++i);    // print = 2 , because value become 1 in previous
