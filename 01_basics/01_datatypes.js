1d68cecc1c0949d8add79b02fbe3b935f48ade12
Mohd Sadiq Ameen <mohdsadiqameen989@gmail.com>
2024-01-26 14:09:10 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"     // treat all js code as newer version 
+
+/*
+number = range - 2 to the power n
+bigint
+string = " "
+boolean - true/false
+null - empty value/ value aaee hi nahi hai
+undefined - come when we havent given the variable the value
+symbol - used for uniqueness ( in React)
+
+object 
+
+*/
+
+
+let age = "2";
+
+// typeof function
+
+console.log(typeof null); // type - object
+console.log(typeof undefined);// type - undefiend
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountNickname = "Carry"
+let accountName = 1234;
+var accountEmail = "man@gmail.com"
+
+console.table([accountNickname , accountName , accountEmail]);
+
+// for running code on terminal node foldername/filename
+
+// console.table for printing ek saath many variables
+
+/* 
+var didnt used now because of 
+problem in block scoping and funtional scope  
+*/
+
+
+
