cc0679a6573f1e94ffc7ee6b1bb4729849d62e34
Mohd Sadiq Ameen <mohdsadiqameen989@gmail.com>
2024-01-27 18:45:53 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,14 @@
+const name = "Sadiq"
+const age = 20;
+
+// printf in " double quotes " , format of writing string + variable
+// old format
+
+console.log("Name is " + name +  " and age is " + age + "\n2nd line")
+
+// printf in `Backticks ` , format of writing string + variable
+// this is callled string interpolation , is it better
+
+console.log(`Name is ${name} and age is ${age} `)
+console.log(`Ready`)
+
