f787d9c6ee38a7db5e43a83985f57a09ec1134b8
Mohd Sadiq Ameen <mohdsadiqameen989@gmail.com>
2024-01-30 05:37:03 +0000

    math

--- 01_basics/06_nums_math.js
+++ 01_basics/06_nums_math.js
@@ -24,4 +24,27 @@ console.log(num.toLocaleString('en-IN'));   // output: 10,00,00,000 - Indian num
 
 // ****************** MATHS library *******************************************************************************
 
+
+console.log(Math);
+console.log(Math.abs(-76));      // output : 76 , conv -ve to +ve
+console.log(Math.round(4.4));    // output : 4 , roundoff of number before decimal
+console.log(Math.round(4.5));    // output : 5 , roundoff of number before decimal
+console.log(Math.round(4.6));    // output : 5 , roundoff of number before decimal
+console.log(Math.round(55.49));  // output : 55
+console.log(Math.round(55.50));  // output : 56
+
+console.log(Math.min(4,3,5,6,-4));  // output : -4
+console.log(Math.max(4,3,5,6,-4));  // output : 6
+
+console.log(Math.ceil(4.2));     // output : 5 , we r deciding the roundoff where to go ( ceil = higher )
+console.log(Math.floor(4.9));    // output : 4 , floor = lower value  
+
+
+console.log(Math.random());            // output : random value between 0 & 1
+console.log((Math.random()*10) + 1) ;  // output : random value between 1 to 10 with decimals
+console.log(Math.floor((Math.random()*10) + 1)) ;  // output : random value between 1 to 10 with no decimals
 */
+const min = 1
+const max = 6
+
+console.log(Math.floor(Math.random()* (max - min +1)) + 1); // formula , example shown here is dice random numbers
