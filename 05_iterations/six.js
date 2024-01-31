c00adf10d1598fb32528bc49ef2867d584f6cb7e
Mohd Sadiq Ameen <mohdsadiqameen989@gmail.com>
2024-01-31 09:25:05 +0000

    filter

--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -40,12 +40,47 @@ const nums = [ 1,2,3,4,5,6,7,8,9,10];
 
 const table = []
 
-const run = nums.forEach((item)=>{            // output:6 7 8 9 10  --(each diff line )
+const run = nums.forEach((item)=>{
     if (item > 5) {
         table.push(item);                     // .push -- push items into array/something eg: array.push(item)
     } 
 })
 
-console.log(table);  
+console.log(table);                          // output : [ 6, 7, 8, 9, 10 ]
+
+
+let books = [
+    { 
+        title: 'To Kill a Mockingbird', 
+        genre: 'Fiction', 
+        publisher: 'J. B. Lippincott & Co.', 
+        publishYear: 1960 
+    },
+    { 
+        title: 'The Great Gatsby', 
+        genre: 'Fiction', 
+        publisher: 'Charles Scribner\'s Sons', 
+        publishYear: 1925 
+    },
+    { 
+        title: '1984', 
+        genre: 'Science Fiction', 
+        publisher: 'Secker & Warburg', 
+        publishYear: 1949 
+    }
+];
+
+let show = books.filter((item)=> {
+    return item.genre === 'Fiction'
+})
+
+ console.log(show);                   // output : whole information of element whose genre = "fiction"
 */
 
+console.log('------');
+
+show = books.filter((item)=>{          // use the variable show = 2times , use of let 
+    return item.publishYear >= 1960 
+})
+
+console.log(show); 
